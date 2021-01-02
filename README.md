0fc083a1766a7993661df874234a33a90bfe92f4
Eshanika-A <73386278+Eshanika-A@users.noreply.github.com>
2021-01-02 15:59:17 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c23-esha
+# TopplingBoxes
+Toppling Boxes
---
+++ box.js
@@ -0,0 +1,27 @@
+class Box {
+constructor(x,y,width,height){
+    var box_options={
+      restitution:0.8,
+      friction:0.8
+    }
+  
+    this. box= Bodies.rectangle(x,y,width,height,box_options);
+    this.width=width;
+    this.height=height;
+    World.add(world,this.box);
+      
+}
+
+display(){
+    var pos=this.box.position;
+    var angle=this.box.angle;
+    push();
+    translate(pos.x,pos.y)
+    rotate(angle)
+    rectMode(CENTER); 
+    fill("white")
+    rect(0,0,this.width,this.height);
+    pop();
+}
+
+}
